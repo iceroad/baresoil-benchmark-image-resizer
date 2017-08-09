dee57f77e5502a2241b399e9445502fcdf72648a
Mayank Lahiri <mayank@iceroad.io>
2017-08-09 13:38:21 -0700

    Minor, comments.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+#!/bin/bash -ex
+#
+# Test cluster instance setup script
+#
+# This file will be executed on each instance in the
+# test cluster. It can be used to perform initial
+# OS configuration after a test cluster is created.
+#
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install -y npm build-essential curl
